f9508b1483320ca20abcd2383d7f3f300241f0bb
aurastudentclub <aura.studentclub@gmail.com>
2026-03-05 21:54:30 +0530

    Updated aura project files

---
+++ app.js
@@ -0,0 +1,50 @@
+const createError = require('http-errors');
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
+
+const indexRouter = require('./routes/index');
+const usersRouter = require('./routes/users');
+
+const app = express();
+
+// Optional MongoDB setup if needed later
+// const mongoose = require('mongoose');
+// mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/aura-club', {
+//   useNewUrlParser: true,
+//   useUnifiedTopology: true
+// });
+
+// ===== View Engine Setup =====
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade'); // Consider changing to EJS or serving HTML directly
+
+// ===== Middleware =====
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+
+// ===== Static Files =====
+app.use(express.static(path.join(__dirname, 'public')));
+
+// ===== Routes =====
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+
+// ===== 404 Handler =====
+app.use(function(req, res, next) {
+  next(createError(404));
+});
+
+// ===== Error Handler =====
+app.use(function(err, req, res, next) {
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  res.status(err.status || 500);
+  res.render('error'); // Make sure views/error.jade exists
+});
+
+module.exports = app;
